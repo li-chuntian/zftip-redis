d8d87646a47bc81111409dda81803d68fad1f4aa
lct <980908645@qq.com>
2022-05-13 13:45:31 +0800

    正常流程

--- src/main/java/com/Parsing/utils/MenaceInfoTask.java
+++ src/main/java/com/Parsing/utils/MenaceInfoTask.java
@@ -21,8 +21,8 @@ public class MenaceInfoTask extends TimerTask {
 
     private final RedisController redisController;
 
-    //    private final List<String> areaNameList = Arrays.asList("changde","chongqing","jiangsu","guizhou","henan","jingmen","yunnan","guangdong","kunming","hunan","hebei","sichuan","guiyang");
-    private final List<String> areaNameList = Arrays.asList("changde", "chongqing");
+        private final List<String> areaNameList = Arrays.asList("changde","chongqing","jiangsu","guizhou","henan","jingmen","yunnan","guangdong","kunming","hunan","hebei","sichuan","guiyang");
+//    private final List<String> areaNameList = Arrays.asList("changde", "chongqing");
 
     public MenaceInfoTask(LostAssetsService lostAssetsService, IZipAnalyseService zipAnalyseService, RedisController redisController) {
         this.lostAssetsService = lostAssetsService;
@@ -37,8 +37,9 @@ public class MenaceInfoTask extends TimerTask {
         String queryStartTime = DateUtil.beginOfDay(yesterday).toString();
         String queryEndTime = DateUtil.endOfDay(yesterday).toString();
         logger.info("任务开始, 本次查询时间段: " + queryStartTime + " - " + queryEndTime);
-        // 创建本次任务压缩包存放目录
-        FebsConstant.UNCOMPRESS = "D:\\cache\\" + DateUtil.format(new Date(), new SimpleDateFormat("yyyy-MM-dd-hhmm")) + "\\";
+        // 创建本次任务压缩包存放目录--
+        FebsConstant.UNCOMPRESS = "./cache/" + DateUtil.format(new Date(), new SimpleDateFormat("yyyy-MM-dd")) + "/";
+//        FebsConstant.UNCOMPRESS = "D:\\cache\\" + DateUtil.format(new Date(), new SimpleDateFormat("yyyy-MM-dd-hhmm")) + "\\";
 
         ArrayList<String> filePathList = new ArrayList<>();
         for (String areaName : areaNameList) {
@@ -55,10 +56,10 @@ public class MenaceInfoTask extends TimerTask {
             List<List<Map<String, Object>>> zipFileContent = zipAnalyseService.getZipFileContent(filePath, uncompress, FebsConstant.QIHOO_ASSETS_API_KEY);
             System.out.println("导入数据: " + zipFileContent);
             //        3.将数据存储绕redis
-//            if (zipFileContent != null && !zipFileContent.isEmpty()) {
-//                logger.info(zipFileContent.toString());
-//                redisController.saveZipDataToRedis(zipFileContent);
-//            }
+            if (zipFileContent != null && !zipFileContent.isEmpty()) {
+                logger.info(zipFileContent.toString());
+                redisController.saveZipDataToRedis(zipFileContent);
+            }
         }
 
 
